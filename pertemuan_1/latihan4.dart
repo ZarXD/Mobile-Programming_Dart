849e0115397c019177921c8dfdd394e4a2a87b79
ZarXD <banghads09@gmail.com>
2025-08-28 21:38:08 +0700

    Update 🌳

--- pertemuan_1/latihan4.dart
+++ pertemuan_1/latihan4.dart
@@ -7,8 +7,18 @@ type_data nama_function(parameter){}
 void sapa() {
   print("Halo apa kabar?");
 }
+
 // Function mengembalikan nilai
+double diskon(double harga, double persen) {
+  return harga * persen / 100;
+}
 
 void main() {
   // Function
+  sapa();
+
+  double hrg = 100000;
+  double persenDiskon = 5;
+
+  print(hrg - diskon(hrg, persenDiskon));
 }
